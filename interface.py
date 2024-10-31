dd4fff535fd97f498707abdb998552b996112b2a
guidng <guiguidonega@gmail.com>
2024-10-30 22:43:30 -0300

    Mudança de botões e musica(arrumar)

--- interface.py
+++ interface.py
@@ -17,10 +17,20 @@ clock = pygame.time.Clock()
 FPS=60
 countloadbar=0
 
+# Carregar a música de fundo
+pygame.mixer.music.load("trilhas_sonoras/trilha_sonora_tela_inicial.mp3")  # Substitua pelo caminho do seu arquivo de música
+pygame.mixer.music.set_volume(0.1)  # Define o volume da música (0.0 a 1.0)
+pygame.mixer.music.play(-1) 
+
+
+
+
+
 # Loop principal
 while game:
 
     clock.tick(FPS)
+    
 
     if current_screen == 'tela carregamento':
         frames+=1
@@ -65,12 +75,15 @@ while game:
             window.blit(text, (600-textrect[2]/2, 530))
 
 
+    
     if current_screen == 'tela inicio':
+        
         # Trata eventos
         for event in pygame.event.get():
             # Verifica consequências
             if event.type == pygame.QUIT:
                 game = False
+
 
         # Gera imagem
         BGSSimage=pygame.image.load('images/BGtelainicial.png')
@@ -90,20 +103,11 @@ while game:
         Arcimage_rect.center=(width/2,(height/2)-150)
         window.blit(Arcimage, Arcimage_rect)
 
-        # Cria botão do play e musica:
-        # Cria círculo
-        radiusplayb=50
-        centerplayb=((width/2)-100,(height/2)+70)
-        colorplayb=(214,217,54)
-        pygame.draw.circle(window, colorplayb, centerplayb, radiusplayb)
-        radiussetb=50
-        centersetb=((width/2)+100,(height/2)+70)
-        colorsetb=(214,217,54)
-        pygame.draw.circle(window, colorsetb, centersetb, radiussetb) 
+        
         
         # Importa Botões:
-        Muteimage = pygame.image.load('images/BotaoMute.png')
-        Volumeimage = pygame.image.load('images/Botaovolume.png')
+        Muteimage = pygame.image.load('images/mudo.png')
+        Volumeimage = pygame.image.load('images/som_ligado.png')
         if sound==False:
             Soundimage=Muteimage
         else:
@@ -113,26 +117,29 @@ while game:
         #         sound=True
         #     else:
         #         sound=False
-         
+        
+        # Cria botão do play e musica:
+
         Soundimagewidth = 100
         Soundimageheight = 100
         Soundimage = pygame.transform.scale(Soundimage, (Soundimagewidth, Soundimageheight))
-        Soundimage_rect=Soundimage.get_rect()
-        Soundimage_rect.center=(centersetb)
+        Soundimage_rect=Soundimage.get_rect( )
+        Soundimage_rect.bottom = window.get_height()
+        Soundimage_rect.left = 0
         window.blit(Soundimage, Soundimage_rect)
 
-        Playimage = pygame.image.load('images/Botaoplay.png')
-        Playimagewidth = 70
-        Playimageheight = 70
+        Playimage = pygame.image.load('images/botao_play.png')
+        Playimagewidth = 300
+        Playimageheight = 300
         Playimage = pygame.transform.scale(Playimage, (Playimagewidth, Playimageheight))
         Playimage_rect=Playimage.get_rect()
-        Playimage_rect.center=((width/2)-98,(height/2)+70)
+        Playimage_rect.center=((width/2),(height/2))
         window.blit(Playimage, Playimage_rect)
 
         # confere se botão do volume foi apertado 
-
+     # Inicia a música apenas quando a tela de carregamento for concluída
     # Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
-
+ 
 # Finalização
 pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
